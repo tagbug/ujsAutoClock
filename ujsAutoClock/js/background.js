e89ceeb55409942acfc02922a80ff6a0cb98a114
tagbug <41150861+tagbug@users.noreply.github.com>
2021-02-08 06:31:17 -0600

    优化

--- ujsAutoClock/js/background.js
+++ ujsAutoClock/js/background.js
@@ -22,6 +22,14 @@ function createTimer () {
         nextRunDate.setDate(nextRunDate.getDate() + 1);
     }
     nextRunDate.setHours(time[0], time[1]);
+    chrome.notifications.create(null, {
+        type: 'basic',
+        iconUrl: 'img/icon.png',
+        title: 'ujs自动健康打卡',
+        message: '定时任务创建成功！下次打卡时间：' + nextRunDate.format("yyyy-MM-dd hh:mm:ss"),
+        requireInteraction: true,
+        priority: 2
+    });
     checker = window.setInterval(function () {
         if (nextRunDate - new Date() < 0) {
             statusCount = 0;
@@ -353,3 +361,27 @@ chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
         }
     }
 });
+Date.prototype.format = function (fmt) {
+    var o = {
+        "M+": this.getMonth() + 1,                 //月份
+        "d+": this.getDate(),                    //日
+        "h+": this.getHours(),                   //小时
+        "m+": this.getMinutes(),                 //分
+        "s+": this.getSeconds(),                 //秒
+        "q+": Math.floor((this.getMonth() + 3) / 3), //季度
+        "S": this.getMilliseconds()             //毫秒
+    };
+
+    if (/(y+)/.test(fmt)) {
+        fmt = fmt.replace(RegExp.$1, (this.getFullYear() + "").substr(4 - RegExp.$1.length));
+    }
+
+    for (var k in o) {
+        if (new RegExp("(" + k + ")").test(fmt)) {
+            fmt = fmt.replace(
+                RegExp.$1, (RegExp.$1.length == 1) ? (o[k]) : (("00" + o[k]).substr(("" + o[k]).length)));
+        }
+    }
+
+    return fmt;
+}
